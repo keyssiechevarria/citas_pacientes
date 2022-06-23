9761894fb08cc07bd7db29f5d94150e03fb38e21
keyssi-ea <keyssieche.20@gmail.com>
2022-06-22 22:47:16 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+  function App(){
+  // Parte 1- Antes del return funciones o validaciones en JS
+    const sumar = () => {
+      // const numero = 5;
+      console.log(2+5)
+    }
+    sumar();
+  // Parte 2 - Return - JS en llaves
+    return(
+      <>
+        <div>
+          <h1>Hola mundo c:</h1>
+          <h2>Mi primera línea con React</h2>
+          <p>holaa</p>
+
+        </div>
+        <div>
+        <h1>Hola mundo c:</h1>
+        <h2>Mi primera línea con React</h2>
+        <h3>holaa</h3>
+
+      </div>
+     </>
+    )
+  }
+    
+
+export default App
